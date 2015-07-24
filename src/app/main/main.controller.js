6aa5c57034ced1eab1dbfe83511b90544392f0b3
Remouille <remi.sala@gmail.com>
2015-07-24 10:57:50 +0200

    Injection data via angular

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,40 +1,88 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('nathy')
-    .controller('MainController', MainController);
+	angular
+		.module('nathy')
+		.controller('MainController', MainController);
 
-  /** @ngInject */
-  function MainController($timeout, $log, webDevTec, $scope) {
-    var vm = this;
-    $log.log('Controller : MainController');
+	/** @ngInject */
+	function MainController($timeout, $log, webDevTec, $scope) {
+		var vm = this;
+		$log.log('Controller : MainController');
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1436890484919;
-    // vm.showToastr = showToastr;
+		vm.awesomeThings = [];
+		vm.classAnimation = '';
+		vm.creationDate = 1436890484919;
+		// vm.showToastr = showToastr;
 
-    activate();
+		activate();
  
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
+		function activate() {
+			getWebDevTec();
+			$timeout(function() {
+				vm.classAnimation = 'rubberBand';
+			}, 4000);
+		}
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
+		function getWebDevTec() {
+			vm.awesomeThings = webDevTec.getTec();
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+			angular.forEach(vm.awesomeThings, function(awesomeThing) {
+				awesomeThing.rank = Math.random();
+			});
+		}
 
-    $scope.removeFocus = function() {
-      angular.element('md-card').removeClass('active md-whiteframe-z4');
-      angular.element('#layer').removeClass('active');
-    };
-  }
+		$scope.removeFocus = function() {
+			angular.element('md-card').removeClass('active md-whiteframe-z4');
+			angular.element('#layer').removeClass('active');
+		};
+
+		$scope.philos = [
+			{ title: 'Home', image: '/assets/images/suissa.jpg' },
+			{ title: 'Home', image: '/assets/images/sketch.jpg' },
+			{ title: 'Home', image: '/assets/images/details.jpg' },
+			{ title: 'Home', image: '/assets/images/beranger.jpg' },
+			{ title: 'Home', image: '/assets/images/sdb.jpg' },
+			{ title: 'Home', image: '/assets/images/silencio.jpg' }
+		];
+
+		$scope.news = [
+			{ title: 'Petit titre', 
+				desc: 'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene\'s musical language: feel it.',
+				image: '/assets/images/actu1.jpg',
+				link:'#'
+			},
+			{ title: 'Mon super titre',
+				desc: 'breakthrough song and the first single from Paracosm share the two most important words in Ernest',
+				image: '/assets/images/actu2.jpg',
+				link:''
+			},
+			{ title: 'Bonjour Bonjour',
+				desc: 'Song and the first single from Paracosm share the two most important words in Ernest',
+				image: '/assets/images/actu3.jpg',
+				link:''
+			
+			}
+		];
+
+		$scope.services = [
+			{ title: 'Petit titre', 
+				desc: 'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene\'s musical language: feel it.',
+				image: '/assets/images/Glyphs1.jpg'
+			},
+			{ title: 'Mon super titre',
+				desc: 'breakthrough song and the first single from Paracosm share the two most important words in Ernest',
+				image: '/assets/images/Glyphs2.jpg'
+			},
+			{ title: 'Bonjour Bonjour',
+				desc: 'Song and the first single from Paracosm share the two most important words in Ernest',
+				image: '/assets/images/Glyphs3.jpg'
+			},
+			{ title: 'Bonjour Bonjour',
+				desc: 'Song and the first single from Paracosm share the two most important words in Ernest',
+				image: '/assets/images/Glyphs4.jpg'
+			}
+		];
+
+	}
 })();
